2e7da51090e28cab16505fa481c489f90254c160
Jarvan <j532547613@gmail.com>
2023-08-15 11:47:35 +0800

    feat: init

--- src/views/rules/index.js
+++ src/views/rules/index.js
@@ -1,107 +1,290 @@
-import React, {Component} from 'react';
-import {Query, Builder, Utils as QbUtils} from 'react-awesome-query-builder';
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  Query, Builder, Utils, 
+} from "react-awesome-query-builder";
 import throttle from "lodash/throttle";
+import loadConfig from "./config";
+// import loadedInitValue from "./init_value";
+// import loadedInitLogic from "./init_logic";
+import clone from "clone";
 
-import AntdConfig from 'react-awesome-query-builder/lib/config/antd';
-// import 'antd/dist/antd.css'; // or import "react-awesome-query-builder/css/antd.less";
-import "react-awesome-query-builder/css/antd.less";
+const stringify = JSON.stringify;
+const {
+  elasticSearchFormat,
+  jsonLogicFormat,
+  queryString,
+  _mongodbFormat,
+  _sqlFormat,
+  _spelFormat,
+  getTree,
+  checkTree,
+  loadTree,
+  uuid,
+  // loadFromJsonLogic,
+  loadFromSpel,
+  isValidTree
+} = Utils;
+const preStyle = { backgroundColor: "darkgrey", margin: "10px", padding: "10px" };
+const preErrorStyle = { backgroundColor: "lightpink", margin: "10px", padding: "10px" };
 
-import 'react-awesome-query-builder/lib/css/styles.css';
-import 'react-awesome-query-builder/lib/css/compact_styles.css'; //optional, for more compact styles
+const emptyInitValue = {id: uuid(), type: "group"};
+const loadedConfig = loadConfig();
+// let initValue = loadedInitValue && Object.keys(loadedInitValue).length > 0 ? loadedInitValue : emptyInitValue;
+// const initLogic = loadedInitLogic && Object.keys(loadedInitLogic).length > 0 ? loadedInitLogic : undefined;
+let initValue = emptyInitValue;
+let initTree;
+initTree = checkTree(loadTree(initValue), loadedConfig);
+// initTree = checkTree(loadFromJsonLogic(initLogic, loadedConfig), loadedConfig); // <- this will work same  
 
-// Choose your skin (ant/material/vanilla):
-const InitialConfig = AntdConfig; // or MaterialConfig or MuiConfig or BootstrapConfig or BasicConfig
 
-// You need to provide your own config. See below 'Config format'
-const config = {
-  ...InitialConfig,
-  fields: {
-    qty: {
-        label: 'Qty',
-        type: 'number',
-        fieldSettings: {
-            min: 0,
-        },
-        valueSources: ['value'],
-        preferWidgets: ['number'],
-    },
-    price: {
-        label: 'Price',
-        type: 'number',
-        valueSources: ['value'],
-        fieldSettings: {
-            min: 10,
-            max: 100,
-        },
-        preferWidgets: ['slider', 'rangeslider'],
-    },
-    color: {
-        label: 'Color',
-        type: 'select',
-        valueSources: ['value'],
-        fieldSettings: {
-          listValues: [
-            { value: 'yellow', title: 'Yellow' },
-            { value: 'green', title: 'Green' },
-            { value: 'orange', title: 'Orange' }
-          ],
-        }
-    },
-    is_promotion: {
-        label: 'Promo?',
-        type: 'boolean',
-        operators: ['equal'],
-        valueSources: ['value'],
-    },
-  }
-};
+// Trick to hot-load new config when you edit `config.tsx`
+const updateEvent = new CustomEvent("update", { detail: {
+  config: loadedConfig,
+  _initTree: initTree,
+  _initValue: initValue,
+} });
+window.dispatchEvent(updateEvent);
 
-// You can load query value from your backend storage (for saving see `Query.onChange()`)
-const queryValue = {"id": QbUtils.uuid(), "type": "group"};
 
-class DemoQueryBuilder extends Component {
-    state = {
-      tree: QbUtils.checkTree(QbUtils.loadTree(queryValue), config),
-      config: config
+const DemoQueryBuilder = () => {
+  const memo = {};
+
+  const [state, setState] = useState({
+    tree: initTree, 
+    config: loadedConfig,
+    spelStr: "",
+    spelErrors: []
+  });
+
+  useEffect(() => {
+    window.addEventListener("update", onConfigChanged);
+    return () => {
+      window.removeEventListener("update", onConfigChanged);
     };
-    
-    render = () => (
-      <div>
-        <Query
-            {...config} 
-            value={this.state.tree}
-            onChange={this.onChange}
-            renderBuilder={this.renderBuilder}
-        />
-        {this.renderResult(this.state)}
-      </div>
-    )
+  });
 
-    renderBuilder = (props) => (
-      <div className="query-builder-container" style={{padding: '10px'}}>
+
+  const onConfigChanged = (e) => {
+    const {detail: {config, _initTree, _initValue}} = e
+    console.log("Updating config...");
+    setState({
+      ...state,
+      config,
+    });
+    initTree = _initTree;
+    initValue = _initValue;
+  };
+
+  const switchShowLock = () => {
+    const newConfig = clone(state.config);
+    newConfig.settings.showLock = !newConfig.settings.showLock;
+    setState({...state, config: newConfig});
+  };
+
+  const resetValue = () => {
+    setState({
+      ...state,
+      tree: initTree, 
+    });
+  };
+
+  const validate = () => {
+    setState({
+      ...state,
+      tree: checkTree(state.tree, state.config)
+    });
+  };
+
+  const onChangeSpelStr = (e) => {
+    const spelStr = e.target.value;
+    setState({
+      ...state,
+      spelStr
+    });
+  };
+
+  const importFromSpel = () => {
+    const [tree, spelErrors] = loadFromSpel(state.spelStr, state.config);
+    setState({
+      ...state, 
+      tree: tree ? checkTree(tree, state.config) : state.tree,
+      spelErrors
+    });
+  };
+
+  const clearValue = () => {
+    setState({
+      ...state,
+      tree: loadTree(emptyInitValue), 
+    });
+  };
+  const renderBuilder = useCallback((bprops) => {
+    memo._actions = bprops.actions;
+    return (
+      <div className="query-builder-container" style={{padding: "10px"}}>
         <div className="query-builder qb-lite">
-            <Builder {...props} />
+          <Builder {...bprops} />
         </div>
       </div>
-    )
+    );
+  // eslint-disable-next-line
+  }, []);
+  const onChange = useCallback((immutableTree, config, actionMeta) => {
+    if (actionMeta)
+      console.info(actionMeta);
+    memo.immutableTree = immutableTree;
+    memo.config = config;
+    updateResult();
+  // eslint-disable-next-line
+  }, []);
 
-    renderResult = ({tree: immutableTree, config}) => (
-      <div className="query-builder-result">
-          <div>Query string: <pre>{JSON.stringify(QbUtils.queryString(immutableTree, config))}</pre></div>
-          <div>MongoDb query: <pre>{JSON.stringify(QbUtils.mongodbFormat(immutableTree, config))}</pre></div>
-          <div>SQL where: <pre>{JSON.stringify(QbUtils.sqlFormat(immutableTree, config))}</pre></div>
-          <div>JsonLogic: <pre>{JSON.stringify(QbUtils.jsonLogicFormat(immutableTree, config))}</pre></div>
+  const updateResult = throttle(() => {
+    setState(prevState => ({...prevState, tree: memo.immutableTree, config: memo.config}));
+  }, 100);
+
+
+  const renderResult = ({tree: immutableTree, config}) => {
+    const isValid = isValidTree(immutableTree);
+    const treeJs = getTree(immutableTree);
+    const {logic, data: logicData, errors: logicErrors} = jsonLogicFormat(immutableTree, config);
+    const [spel, spelErrors] = _spelFormat(immutableTree, config);
+    const queryStr = queryString(immutableTree, config);
+    const humanQueryStr = queryString(immutableTree, config, true);
+    const [sql, sqlErrors] = _sqlFormat(immutableTree, config);
+    const [mongo, mongoErrors] = _mongodbFormat(immutableTree, config);
+    const elasticSearch = elasticSearchFormat(immutableTree, config);
+
+    return (
+      <div>
+        {isValid ? null : <pre style={preErrorStyle}>{"Tree has errors"}</pre>}
+        <br />
+        <div>
+        spelFormat: 
+          { spelErrors.length > 0 
+            && <pre style={preErrorStyle}>
+              {stringify(spelErrors, undefined, 2)}
+            </pre> 
+          }
+          <pre style={preStyle}>
+            {stringify(spel, undefined, 2)}
+          </pre>
+        </div>
+        <hr/>
+        <div>
+        stringFormat: 
+          <pre style={preStyle}>
+            {stringify(queryStr, undefined, 2)}
+          </pre>
+        </div>
+        <hr/>
+        <div>
+        humanStringFormat: 
+          <pre style={preStyle}>
+            {stringify(humanQueryStr, undefined, 2)}
+          </pre>
+        </div>
+        <hr/>
+        <div>
+        sqlFormat: 
+          { sqlErrors.length > 0 
+            && <pre style={preErrorStyle}>
+              {stringify(sqlErrors, undefined, 2)}
+            </pre> 
+          }
+          <pre style={preStyle}>
+            {stringify(sql, undefined, 2)}
+          </pre>
+        </div>
+        <hr/>
+        <div>
+          <a href="http://jsonlogic.com/play.html" target="_blank" rel="noopener noreferrer">jsonLogicFormat</a>: 
+          { logicErrors.length > 0 
+            && <pre style={preErrorStyle}>
+              {stringify(logicErrors, undefined, 2)}
+            </pre> 
+          }
+          { !!logic
+            && <pre style={preStyle}>
+              {"// Rule"}:<br />
+              {stringify(logic, undefined, 2)}
+              <br />
+              <hr />
+              {"// Data"}:<br />
+              {stringify(logicData, undefined, 2)}
+            </pre>
+          }
+        </div>
+        <hr/>
+        <div>
+        mongodbFormat: 
+          { mongoErrors.length > 0 
+            && <pre style={preErrorStyle}>
+              {stringify(mongoErrors, undefined, 2)}
+            </pre> 
+          }
+          <pre style={preStyle}>
+            {stringify(mongo, undefined, 2)}
+          </pre>
+        </div>
+        <hr/>
+        <div>
+        elasticSearchFormat: 
+          <pre style={preStyle}>
+            {stringify(elasticSearch, undefined, 2)}
+          </pre>
+        </div>
+        <hr/>
+        <div>
+        Tree: 
+          <pre style={preStyle}>
+            {stringify(treeJs, undefined, 2)}
+          </pre>
+        </div>
+        {/* <hr/>
+      <div>
+        queryBuilderFormat: 
+          <pre style={preStyle}>
+            {stringify(queryBuilderFormat(immutableTree, config), undefined, 2)}
+          </pre>
+      </div> */}
       </div>
-    )
+    );
+  };
 
-    onChange = throttle((immutableTree, config) => {
-      // Tip: for better performance you can apply `throttle` - see `examples/demo`
-      this.setState({tree: immutableTree, config: config});
-    
-      const jsonTree = QbUtils.getTree(immutableTree);
-      console.log(jsonTree);
-      // `jsonTree` can be saved to backend, and later loaded to `queryValue`
-    }, 300)
-}
+  return (
+    <div>
+      <div>
+        <button onClick={resetValue}>reset</button>
+        <button onClick={clearValue}>clear</button>
+        <button onClick={validate}>validate</button>
+        <button onClick={switchShowLock}>show lock: {state.config.settings.showLock ? "on" : "off"}</button>
+      </div>
+      
+      <Query
+        {...state.config}
+        value={state.tree}
+        onChange={onChange}
+        renderBuilder={renderBuilder}
+      />
 
-export default DemoQueryBuilder
+      <div className="query-import-spel">
+        SpEL:
+        <input type="text" size={150} value={state.spelStr} onChange={onChangeSpelStr} />
+        <button onClick={importFromSpel}>import</button>
+        <br />
+        { state.spelErrors.length > 0 
+            && <pre style={preErrorStyle}>
+              {stringify(state.spelErrors, undefined, 2)}
+            </pre> 
+        }
+      </div>
+
+      <div className="query-builder-result">
+        {renderResult(state)}
+      </div>
+    </div>
+  );
+};
+
+
+export default DemoQueryBuilder;

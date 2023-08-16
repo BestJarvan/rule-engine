31a8fb66b28c91af560d6bd8f397065fb3aa66de
Jarvan <j532547613@gmail.com>
2023-08-16 15:27:35 +0800

    feat: update;

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,12 @@
 import {
-  createBrowserRouter,
+  createHashRouter,
 } from "react-router-dom";
 import Index from '../views/index';
 import ErrorPage from '../views/error'
 import Rules from '../views/rules/index';
 import Home from '../views/home';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <Index />,

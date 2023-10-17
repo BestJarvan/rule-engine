dc94e244835faa4741bd64dcdd05dcb6da56e76e
Jarvan <j532547613@gmail.com>
2023-10-17 11:43:18 +0800

    feat: pre环境

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,7 @@ console.log('process.env.REACT_APP_ENV: ', process.env.REACT_APP_ENV);
 
 const requestMap = {
   'master': 'https://pre.primecare.top',
+  'pre': 'https://uat-pre.primecare.top',
   'serve': 'https://jpicp-test.primecare.top',
 }
 
